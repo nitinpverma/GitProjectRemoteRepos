8ffec2fd9d6ddd4d28a368065e8123883c533142
Nitin <cs.nitinverma@gmail.com>
2017-03-19 20:27:13 +0530

    myclass

---
+++ MY/src/myPackage/Myclass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package myPackage;
+
+/**
+ * @author Nitin
+ *
+ */
+public class Myclass {
+
+	/**
+	 * 
+	 */
+	public Myclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
